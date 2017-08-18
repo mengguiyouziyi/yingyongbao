7ecb4178f1a47b9a3a99cd46d5ec33057480c6bb
mengguiyouziyi <775618369@qq.com>
2017-08-18 19:10:51 +0800

    1

--- app/app/middlewares.py
+++ app/app/middlewares.py
@@ -16,20 +16,20 @@ proxyServer = "http://proxy.abuyun.com:9020"
 # proxyPass = "5B75712BA415792F"
 
 # 2
-# proxyUser = "H85EDB7IE259O1KD"
-# proxyPass = "F8A6E39AA897BAC9"
+# proxyUser = "H256J792G0477J3D"
+# proxyPass = "A978AA975C1C8E59"
 
 # 3
-proxyUser = "F8A6E39AA897BAC9"
-proxyPass = "46A2F86F814A69C6"
+proxyUser = "HDJYRWK2113M79ED"
+proxyPass = "0FE38BB4282AFC2B"
 
 # 4
-# proxyUser = "H2P8I34Q13A598FD"
-# proxyPass = "DEEB8FEDBC497E2D"
+# proxyUser = "H9269C63N6C305PD"
+# proxyPass = "B4BA11856FD0D4E3"
 
 # 5
-# proxyUser = "H13A56PM76U9F43D"
-# proxyPass = "470D49B9708E962F"
+# proxyUser = "H245OL3592LCHU6D"
+# proxyPass = "3C0CEF6238CAF281"
 
 
 # for Python3
